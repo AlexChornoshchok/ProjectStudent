954685106bbf7173fce6e74897d50c7f1888d897
User <aleksblacke@gmail.com>
2018-02-01 13:23:16 +0200

    2018_02_01 J

--- src/academy/itcloud/aleksandr/Main.java
+++ src/academy/itcloud/aleksandr/Main.java
@@ -2,66 +2,18 @@ package academy.itcloud.aleksandr;
 
 
 import academy.itcloud.aleksandr.cli.Console;
+import academy.itcloud.aleksandr.system.CreateBD;
 
 public class Main {
 
     public static void main(String... args) {
 
-//        PersonMap.addTrainer("Albert ", "Einstein", 139);
-//        PersonMap.addStudent("Ivan", "Ivanov ", 27);
-//        PersonMap.addStudent("Petr", "Petrov ", 25);
-//        PersonMap.addStudent("Sidor", "Sidorov ", 22);
-//        PersonMap.addStudent("Aleksandr", "Aleksandrov ", 24);
-//        PersonMap.addTrainer("Lev ", "Landau", 110);
-//
-//        PersonMap.printAllTrainer();
-//        PersonMap.printAllStudent();
-//        PersonMap.printInfoPerson(1);
-//        PersonMap.printInfoPerson(4);
-//
-//        CourseMap controlCourse = new CourseMap();
-//        controlCourse.createCourse("Basics of programming",
-//                "Learning the basics of programming",
-//                1, "01.02.2018", "01.03.2018",
-//                 DaysOfWeek.MONDAY, DaysOfWeek.THURSDAY, DaysOfWeek.FRIDAY);
-//        controlCourse.createCourse("Java of programming",
-//                "Learning the java of programming",
-//                6, "12.02.2018",
-//                "01.04.2018",
-//                 DaysOfWeek.MONDAY, DaysOfWeek.THURSDAY, DaysOfWeek.FRIDAY);
-//        controlCourse.printCourseList();
-//        controlCourse.createCourse("Java of programming", "Learning the java of programming", 6, "12.02.2018", "01.04.2018");
-//
-//        controlCourse.addTaskOfCourse(1,"Task1", "Task2", "Task3");
-//        controlCourse.addTaskOfCourse(2,"Task-1", "Task-2", "Task-3");
-//        controlCourse.addStudentOfCourse(1, 2);
-//        controlCourse.addStudentOfCourse(1, 3);
-//        controlCourse.addStudentOfCourse(1, 4);
-//        controlCourse.addStudentOfCourse(2, 4);
-//
-////        PersonMap.printInfoPerson(4);
-//        System.out.println();
-//        controlCourse.printNameOfCourse(1);
-//        System.out.println();
-//        controlCourse.printFullInfoOfCourse(1);
-//        System.out.println();
-//        controlCourse.printStudentsFromTheCourse(1);
-//        System.out.println();
-//
-//
-////        controlCourse.printJournalOfCourse(1);
-//
-////        PersonMap.printInfoPerson(2);
-////        controlCourse.removeStudentFromACourse(2,2);
-////        controlCourse.transferOfTheStudent(2,1, 2);
-//
-////        controlCourse.saveJournalInFile(1);
-////        PersonMap.printInfoPerson(2);
 
-/// CONSOLE
+///  ****CONSOLE
 
     Console console = new Console();
     console.start();
+
 
 
     }
